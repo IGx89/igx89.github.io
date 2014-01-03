bc869095462979fc47db015d0638c75b436d7c48
Matthew Lieder <lieder_matthew@hotmail.com>
2014-01-03 14:03:46 -0600

    initial commit

---
+++ CKEditor-ShowProtected-Plugin/config.js
@@ -0,0 +1,14 @@
+
+CKEDITOR.editorConfig = function( config ) {
+	
+	config.extraPlugins = 'showprotected';
+	
+	// Mark Freemarker directives as protected
+	config.protectedSource.push( /\[@[\s\S]*?\/]/g );
+	config.protectedSource.push( /\[#[\s\S]*?]/g );
+	config.protectedSource.push( /\[\/#[\s\S]*?]/g );
+	config.protectedSource.push( /\${[\s\S]*?}/g );
+	
+	// Mark PHP directives as protected
+	config.protectedSource.push(/<\?[\s\S]*?\?>/g);
+};
